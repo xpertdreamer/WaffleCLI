835c5bf82fca2c590f9abdbb0a6450e6d7252811
Alexius <106870568+xpertdreamer@users.noreply.github.com>
2025-11-25 21:49:47 +0300

    Delete templates directory

--- templates/uninstall-templates.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-
-Write-Host "🗑️ Uninstalling WaffleCLI Templates..." -ForegroundColor Yellow
-
-dotnet new uninstall ./wafflecli-basic
-
-Write-Host "✅ Templates uninstalled!" -ForegroundColor Green
